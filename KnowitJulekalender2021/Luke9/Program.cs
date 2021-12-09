e27899c0918d7bb4ae6c376e4be18345924b1219
Terje Wiesener <terje.wiesener@sonat.no>
2021-12-10 00:13:40 +0100

    Luke 9

---
+++ KnowitJulekalender2021/Luke9/Program.cs
@@ -0,0 +1,62 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Utilities;
+
+namespace Luke9
+{
+    public class Program
+    {
+        record YearlyNumbers(int Year, long GiftsRemaining, long NiceKids);
+        YearlyNumbers[] years = new[]
+        {
+            new YearlyNumbers(2019,1854803357,2424154637),
+            new YearlyNumbers(2020,2787141611,2807727397),
+            new YearlyNumbers(2021,1159251923,2537380333),
+        };
+
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            Console.WriteLine($"X (Seek): {p.SolveSeek()}");
+            Console.WriteLine($"X (CRT): {p.SolveCRT()}");
+            BenchmarkRunner.Run<Program>();
+        }
+
+        [Benchmark]
+        public long SolveSeek()
+        {
+            Dictionary<long, int> candidates = new Dictionary<long, int>();
+            for (int giftsPerKid = 1; giftsPerKid <= 2000; giftsPerKid++)
+            {
+                for (int i = 0; i < years.Length; i++)
+                {
+                    var handedOut = giftsPerKid * years[i].NiceKids;
+                    var remainder = years[i].GiftsRemaining;
+                    var sum = handedOut + remainder;
+                    if (candidates.ContainsKey(sum))
+                    {
+                        var count = ++candidates[sum];
+                        if (count == 3) return sum;
+                    }
+                    else candidates.Add(sum, 1);
+                }
+            }
+            return -1;
+        }
+
+        [Benchmark]
+        public long SolveCRT()
+        {
+            var num = years.Select(y => (BigInteger)y.NiceKids).ToArray();
+            var rem = years.Select(y => (BigInteger)y.GiftsRemaining).ToArray();
+            BigInteger x = ChineseRemainderTheorem.Solve(num, rem);
+            return (long)x;
+        }
+
+        
+    }
+}
---
+++ KnowitJulekalender2021/Utilities/ChineseRemainderTheorem.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Numerics;
+
+namespace Utilities
+{
+    public class ChineseRemainderTheorem
+    {
+        // Returns modulo inverse of 'a' with respect to 'm' using extended Euclid Algorithm.
+        // Refer below post for details:
+        // https://www.geeksforgeeks.org/multiplicative-inverse-under-modulo-m/
+        public static BigInteger ModuloInverse(BigInteger a, BigInteger m)
+        {
+            BigInteger m0 = m, t, q;
+            BigInteger x0 = 0, x1 = 1;
+
+            if (m == 1)
+                return 0;
+
+            // Apply extended Euclid Algorithm
+            while (a > 1)
+            {
+                // q is quotient
+                q = a / m;
+                t = m;
+
+                // m is remainder now, process same as euclid's algo
+                m = a % m;
+                a = t;
+                t = x0;
+                x0 = x1 - q * x0;
+                x1 = t;
+            }
+
+            // Make x1 positive
+            if (x1 < 0)
+            {
+                x1 += m0;
+            }
+
+            return x1;
+        }
+
+        // Returns the smallest number x such that:
+        // x % num[0] = rem[0],
+        // x % num[1] = rem[1],
+        // ..................
+        // x % num[k-2] = rem[k-1]
+        // Assumption: Numbers in num[] are pairwise coprime (gcd for every pair is 1)
+        public static BigInteger Solve(BigInteger[] num, BigInteger[] rem)
+        {
+            // Compute product of all numbers
+            BigInteger prod = num[0];
+            for (int i = 1; i < num.Length; i++)
+            {
+                prod *= num[i];
+            }
+
+            BigInteger result = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                var pp = prod / num[i];
+                result += rem[i] * ModuloInverse(pp, num[i]) * pp;
+            }
+
+            return result % prod;
+        }
+    }
+}
