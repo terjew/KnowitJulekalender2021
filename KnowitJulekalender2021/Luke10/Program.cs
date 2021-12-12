49ee585a163dba7fcb5c19d8a59fb6e0f39feddb
Terje Wiesener <terje.wiesener@sonat.no>
2021-12-12 03:19:49 +0100

    Luke 10 og 11

---
+++ KnowitJulekalender2021/Luke10/Program.cs
@@ -0,0 +1,66 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Luke10
+{
+    class Node
+    {
+        public char Name { get; init; }
+        public Dictionary<Node,Node> Occluded { get; set; }
+    }
+    public class Program
+    {
+        private Node[] nodes;
+
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            Console.WriteLine(p.Solve());
+            BenchmarkRunner.Run<Program>();
+        }
+
+        [Benchmark]
+        public int Solve()
+        {
+            Node
+               a = new Node() { Name = 'a' },
+               b = new Node() { Name = 'b' },
+               c = new Node() { Name = 'c' },
+               d = new Node() { Name = 'd' },
+               e = new Node() { Name = 'e' },
+               f = new Node() { Name = 'f' },
+               g = new Node() { Name = 'g' },
+               h = new Node() { Name = 'h' },
+               i = new Node() { Name = 'i' };
+            a.Occluded = new Dictionary<Node, Node>() { { c, b }, { i, e }, { g, d } };
+            b.Occluded = new Dictionary<Node, Node>() { { h, e } };
+            c.Occluded = new Dictionary<Node, Node>() { { a, b }, { g, e }, { i, f } };
+            d.Occluded = new Dictionary<Node, Node>() { { f, e } };
+            e.Occluded = new Dictionary<Node, Node>();
+            f.Occluded = new Dictionary<Node, Node>() { { d, e } };
+            g.Occluded = new Dictionary<Node, Node>() { { a, d }, { c, e }, { i, h } };
+            h.Occluded = new Dictionary<Node, Node>() { { b, e } };
+            i.Occluded = new Dictionary<Node, Node>() { { g, h }, { a, e }, { c, f } };
+            nodes = new[] { a, b, c, d, e, f, g, h, i };
+            return CountPaths(nodes.Single(n => n.Name == 'd'), new HashSet<Node>(), 8);
+        }
+
+        private int CountPaths(Node node, HashSet<Node> visited, int max)
+        {
+            if (visited.Count == max - 1) return 1;
+            visited.Add(node);
+            int count = 0;
+            foreach(var next in nodes)
+            {
+                if (visited.Contains(next)) continue;
+                if (node.Occluded.ContainsKey(next) && !visited.Contains(node.Occluded[next])) continue;
+                count += CountPaths(next, visited, max);
+            }
+            visited.Remove(node);
+            return count;
+        }
+    }
+}
---
+++ KnowitJulekalender2021/Luke11/Program.cs
@@ -0,0 +1,91 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Luke11
+{
+    record NameInfo(int length, Regex[] regexes);
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            var k = p.Solve();
+            Console.WriteLine(k);
+            BenchmarkRunner.Run<Program>();
+        }
+
+        [Benchmark]
+        public (string, int) Solve()
+        {
+            var names = File.ReadLines("names.txt")
+                .ToDictionary(n => n, n => new NameInfo(n.Length, Permutations(n)
+                                                                    .Select(p => ToRegex(p))
+                                                                    .ToArray()));
+            var lines = File.ReadLines("locked.txt");
+
+            var bestKid = lines
+                .AsParallel()
+                .Select(l => DecodeKid(l, names))
+                .Where(name => name != null)
+                .GroupBy(name => name)
+                .OrderByDescending(grp => grp.Count())
+                .First();
+
+            return (bestKid.Key, bestKid.Count());
+        }
+
+        private string DecodeKid(string line, Dictionary<string, NameInfo> names)
+        {
+            var matches = names
+                    .Select(kvp => new { Name = kvp.Key, Score = BestMatch(line, kvp.Value) })
+                    .Where(p => p.Score != int.MaxValue)
+                    .OrderBy(p => p.Score)
+                    .ToArray();
+            if (matches.Length == 0) return null;
+            var first = matches[0];
+            if (matches.Length == 1 || matches[1].Score > first.Score)
+            {
+                return first.Name;
+            }
+            return null;
+        }
+
+        private static IEnumerable<string> Permutations(string name)
+        {
+            yield return name;
+            for (int i = 0; i < name.Length - 1; i++)
+            {
+                int j = i + 1;
+                var sb = new StringBuilder(name);
+                sb[j] = name[i];
+                sb[i] = name[j];
+                yield return sb.ToString();
+            }
+        }
+
+        private static Regex ToRegex(string permutation)
+        {
+            var reString = string.Join(".*?", permutation.ToCharArray());
+            return new Regex(reString);
+        }
+
+        private static int BestMatch(string line, NameInfo nameInfo)
+        {
+            int min = int.MaxValue;
+            foreach (var re in nameInfo.regexes)
+            {
+                var match = re.Match(line);
+                if (match.Success && match.Length < min) min = match.Length - nameInfo.length;
+            }
+            return min;
+        }
+
+    }
+}
