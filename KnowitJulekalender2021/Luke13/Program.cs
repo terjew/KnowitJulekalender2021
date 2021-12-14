cedea4775935e7a055c8910331f7b77042059f09
Terje Wiesener <terje.wiesener@sonat.no>
2021-12-14 22:15:24 +0100

    Luke14

--- KnowitJulekalender2021/Luke13/Program.cs
+++ KnowitJulekalender2021/Luke13/Program.cs
@@ -6,7 +6,7 @@ namespace Luke13
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Løst i Unity (se eget prosjekt)");
         }
     }
 }
---
+++ KnowitJulekalender2021/Luke14/Program.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Luke14
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            Console.WriteLine(p.SolveRegex());
+            BenchmarkRunner.Run<Program>();
+        }
+
+        static Regex regex = new Regex("(t.{1,5}r.{1,5}o.{1,5}l.{1,5}l)|(^[^n].*(n.{0,2}i.{0,2}s.{0,2}s.{0,2}e).*[^e]$)");
+
+        [Benchmark]
+        public int SolveRegex()
+        {
+            return File.ReadLines("ordliste.txt").AsParallel().Count(l => regex.IsMatch(l));
+        }
+    }
+}
